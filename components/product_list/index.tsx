cbc1b5c1fb47a4a650990d7aa572e79c59e4c724
Lise Carpenter <lise.carpenter@gmail.com>
2022-11-01 12:15:39 +0700

    new component and update wording

---
+++ components/product_list/index.tsx
@@ -0,0 +1,11 @@
+import { faX } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
+import { FunctionComponent } from 'react'
+
+type Props = {}
+
+const ProductList: FunctionComponent<Props> = ({}) => {
+  return <></>
+}
+export default ProductList
