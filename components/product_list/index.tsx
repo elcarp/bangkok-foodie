5aec1d525b31ae6e330f072171b4804326ae3f36
Lise Carpenter <lise.carpenter@gmail.com>
2022-11-01 12:49:26 +0700

    mobile styling

--- components/product_list/index.tsx
+++ components/product_list/index.tsx
@@ -16,8 +16,8 @@ type Props = {
 const ProductList: FunctionComponent<Props> = ({ restaurants }) => {
   console.log(restaurants)
   return (
-    <div className='container mx-auto mt-20'>
-      <div className='grid grid-cols-4 justify-center items-center text-center gap-5'>
+    <div className='container mx-auto mt-20 px-4 lg:px-0'>
+      <div className='grid grid-cols-2 lg:grid-cols-4 justify-center items-center text-center gap-5'>
         {restaurants.map(({ frontmatter, slug }: any) => {
           return (
             <>
