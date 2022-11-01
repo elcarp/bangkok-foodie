9528680325da6506a666f66b224e7a7487de11b9
Lise Carpenter <lise.carpenter@gmail.com>
2022-11-01 14:07:48 +0700

    WIP

--- pages/restaurants/[slug].js
+++ pages/restaurants/[slug].js
@@ -8,17 +8,27 @@ import Link from 'next/link'
 // import styles from './styles.module.css'
 import React from 'react'
 
-export default function PostPage({ frontmatter, slug, contracts }) {
+export default function PostPage({ frontmatter }) {
+  console.log(frontmatter, 'frontmatter?')
   return (
     <>
       <Head>
-        <title>{frontmatter?.title}</title>
+        <title>{frontmatter?.name}</title>
         {/* <link rel='icon' href='/dm-favicon.png' /> */}
         {/* <meta
           name='description'
           content={`The best way to transfer money from Thailand to ${frontmatter?.receiving_country}. Transfer online overseas. One-time one-document registration, flat transfer fee, best rates.`}
         /> */}
       </Head>
+      <h1 className='text-center py-10'>{frontmatter?.name}</h1>
+      <div className='relative w-3/4 lg:w-1/2 mx-auto h-screen-1/2 rounded-xl overflow-hidden'>
+        <Image
+          src={frontmatter?.main_image}
+          alt={frontmatter?.name}
+          layout='fill'
+          objectFit='cover'
+        />
+      </div>
     </>
   )
 }
